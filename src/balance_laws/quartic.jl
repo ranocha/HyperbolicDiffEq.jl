40664339e83004bc8dcb9cce5c9ca64b03cd3770
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-10-26 18:32:55 +0200

    Quartic and GaussLegendre

--- src/balance_laws/quartic.jl
+++ src/balance_laws/quartic.jl
@@ -62,3 +62,61 @@ Compute the energy (L₂ entropy) conservative flux between `uₗ` and `uᵣ` fo
 function (flux::EnergyConservativeFlux)(uₗ::T, uᵣ::T, model::Quartic{T}) where T<:Real
     ((uₗ^2)^2 + uₗ^3*uᵣ + uₗ^2*uᵣ^2 + uₗ*uᵣ^3 + (uᵣ^2)^2) / 5
 end
+
+
+################################################################################
+
+@inline function add_numerical_fluxes_inner_loop2!(du, fluxes, u, balance_law::Quartic,
+                                                    Nx, basis::GaussLegendre,
+                                                    jacx, parallel)
+    Pp1 = length(basis.nodes)
+    Rl = reshape(interpolation_matrix(-1, basis), Pp1)
+    Rr = reshape(interpolation_matrix(+1, basis), Pp1)
+    utmp = zeros(eltype(u), size(u,1))
+    one_5 = 1 / 5
+    two_5 = 2 / 5
+
+    # add numerical fluxes
+    @inbounds for ix in Base.OneTo(Nx)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]
+        end
+        Rul = dot(Rl, utmp)
+        Rur = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]^2
+        end
+        Ru2l = dot(Rl, utmp)
+        Ru2r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]^3
+        end
+        Ru3l = dot(Rl, utmp)
+        Ru3r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = (u[nx,ix]^2)^2
+        end
+        Ru4l = dot(Rl, utmp)
+        Ru4r = dot(Rr, utmp)
+
+        for nx in 1:Pp1
+            du[nx,ix] += (  (fluxes[ix  ]   - two_5 * Ru4l
+                                            - one_5 * Ru2l^2
+                                            - one_5 * Ru2l * Rul^2
+                                            - one_5 * (Rul^2)^2
+                                            - two_5 * u[nx,ix] * Ru3l
+                                            + one_5 * u[nx,ix] * Ru2l * Rul
+                                            + one_5 * u[nx,ix] * Rul^3) * Rl[nx]
+                          - (fluxes[ix+1]   - two_5 * Ru4r
+                                            - one_5 * Ru2r^2
+                                            - one_5 * Ru2r * Rur^2
+                                            - one_5 * (Rur^2)^2
+                                            - two_5 * u[nx,ix] * Ru3r
+                                            + one_5 * u[nx,ix] * Ru2r * Rur
+                                            + one_5 * u[nx,ix] * Rur^3) * Rr[nx]
+                            ) * jacx / basis.weights[nx]
+        end
+    end
+
+    nothing
+end
