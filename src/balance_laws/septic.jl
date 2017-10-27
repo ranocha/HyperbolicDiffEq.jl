fe6220f028d807a920fd0dae067075faa77d72c5
Hendrik Ranocha <h.ranocha@tu-bs.de>
2017-10-27 13:33:30 +0200

    Septic and GaussLegendre

--- src/balance_laws/septic.jl
+++ src/balance_laws/septic.jl
@@ -55,3 +55,92 @@ function (flux::EnergyConservativeFlux)(uₗ::T, uᵣ::T, model::Septic{T}) wher
     ((uₗ^2)^2*uₗ^3 + (uₗ^3)^2*uᵣ + uₗ^2*uₗ^3*uᵣ^2 + (uₗ^2)^2*uᵣ^3 + uₗ^3*(uᵣ^2)^2
     + uₗ^2*uᵣ^2*uᵣ^3 + uₗ*(uᵣ^3)^2 + (uᵣ^2)^2*uᵣ^3) / 8
 end
+
+
+################################################################################
+
+@inline function add_numerical_fluxes_inner_loop2!(du, fluxes, u, balance_law::Septic,
+                                                    Nx, basis::GaussLegendre,
+                                                    jacx, parallel)
+    Pp1 = length(basis.nodes)
+    Rl = reshape(interpolation_matrix(-1, basis), Pp1)
+    Rr = reshape(interpolation_matrix(+1, basis), Pp1)
+    utmp = zeros(eltype(u), size(u,1))
+    one_4 = 1 / 4
+    one_8 = 1 / 8
+
+    # add numerical fluxes
+    @inbounds for ix in Base.OneTo(Nx)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]
+        end
+        Rul = dot(Rl, utmp)
+        Rur = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]^2
+        end
+        Ru2l = dot(Rl, utmp)
+        Ru2r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]^3
+        end
+        Ru3l = dot(Rl, utmp)
+        Ru3r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = (u[nx,ix]^2)^2
+        end
+        Ru4l = dot(Rl, utmp)
+        Ru4r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]^3*u[nx,ix]^2
+        end
+        Ru5l = dot(Rl, utmp)
+        Ru5r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = (u[nx,ix]^3)^2
+        end
+        Ru6l = dot(Rl, utmp)
+        Ru6r = dot(Rr, utmp)
+        for nx in 1:Pp1
+            utmp[nx] = u[nx,ix]^3*(u[nx,ix]^2)^2
+        end
+        Ru7l = dot(Rl, utmp)
+        Ru7r = dot(Rr, utmp)
+
+        for nx in 1:Pp1
+            du[nx,ix] += (  (fluxes[ix  ]   - one_4 * Ru7l
+                                            - one_4 * u[nx,ix] * Ru6l
+                                            - one_4 * u[nx,ix]^2 * Ru5l
+                                            - one_8 * u[nx,ix]^3 * Ru4l
+                                            - one_8 * Rul^3*(Rul^2)^2
+                                            + one_8 * u[nx,ix] * (Rul^3)^2
+                                            - one_8 * Ru2l * Rul^2*Rul^3
+                                            + one_8 * u[nx,ix]^2 * Rul^2*Rul^3
+                                            - one_8 * Ru3l * (Rul^2)^2
+                                            + one_8 * u[nx,ix]^3 * (Rul^2)^2
+                                            - one_8 * Ru4l * Rul^3
+                                            + one_8 * u[nx,ix] * Ru4l * Rul^2
+                                            - one_8 * Ru4l * Ru2l * Rul
+                                            + one_8 * u[nx,ix]^2 * Ru4l * Rul
+                                            - one_8 * Ru4l * Ru3l) * Rl[nx]
+                          - (fluxes[ix+1]   - one_4 * Ru7r
+                                            - one_4 * u[nx,ix] * Ru6r
+                                            - one_4 * u[nx,ix]^2 * Ru5r
+                                            - one_8 * u[nx,ix]^3 * Ru4r
+                                            - one_8 * Rur^3*(Rur^2)^2
+                                            + one_8 * u[nx,ix] * (Rur^3)^2
+                                            - one_8 * Ru2r * Rur^2*Rur^3
+                                            + one_8 * u[nx,ix]^2 * Rur^2*Rur^3
+                                            - one_8 * Ru3r * (Rur^2)^2
+                                            + one_8 * u[nx,ix]^3 * (Rur^2)^2
+                                            - one_8 * Ru4r * Rur^3
+                                            + one_8 * u[nx,ix] * Ru4r * Rur^2
+                                            - one_8 * Ru4r * Ru2r * Rur
+                                            + one_8 * u[nx,ix]^2 * Ru4r * Rur
+                                            - one_8 * Ru4r * Ru3r) * Rr[nx]
+                            ) * jacx / basis.weights[nx]
+        end
+    end
+
+    nothing
+end
