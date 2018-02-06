8d62bbfa4b779cbe920b3b4015c2d1e9742dc88c
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-02-06 08:32:25 +0100

    tests for KeyfitzKranzer

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,5 +8,6 @@ tic()
 @time @testset "Monomial Fluxes" begin include("monomial_fluxes_test.jl") end
 @time @testset "Flux Difference" begin include("flux_difference_test.jl") end
 @time @testset "ENO Reconstruction" begin include("modified_eno_test.jl") end
+@time @testset "Keyfitz-Kranzer System" begin include("balance_laws/keyfitz_kranzer_test.jl") end
 
 toc()
