cfe581fd7975261d5c2b03849d0d929116222572
Hendrik Ranocha <mail@ranocha.de>
2020-01-20 18:57:16 +0300

    update notebooks

--- test/flux_difference_test.jl
+++ test/flux_difference_test.jl
@@ -30,3 +30,26 @@ ode = semidiscretise(semidisc, u₀func, tspan)
 sol = solve(ode, SSPRK104(), dt=max_dt(tspan[1], ode.u0, semidisc), save_everystep=false)
 @inferred evaluate_coefficients(sol.u[end], semidisc)
 x, u = evaluate_coefficients(sol.u[end], semidisc)
+
+
+
+balance_law = HyperbolicDiffEq.Euler{Float64,2}(1.4)
+ϱ₀(x,y)  = 1.
+vx₀(x,y) = sin(x)*cos(y)
+vy₀(x,y) = -cos(x)*sin(y)
+p₀(x,y)  = 100/1.4 + (cos(2x)+cos(2y))*3/16
+u₀(x,y) = conserved_variables(ϱ₀(x,y), vx₀(x,y), vy₀(x,y), p₀(x,y), balance_law)
+xmin=0.; xmax=2π; ymin=0.; ymax=2π
+fvol = KennedyGruberFlux()
+fnum = fvol
+p = 9
+Nx = 5
+Ny = 5
+meshx = UniformPeriodicMesh1D(xmin, xmax, Nx)
+meshy = UniformPeriodicMesh1D(ymin, ymax, Ny)
+basis = LobattoLegendre(p)
+tspan = (0., 14.)
+semidisc = UniformPeriodicFluxDiffDisc2D(balance_law, meshx, meshy, basis, fvol, fnum, true)
+ode = semidiscretise(semidisc, u₀, tspan)
+tstops = range(tspan[1], tspan[end], length=100)
+sol = solve(ode, SSPRK33(), dt=0.1/((2p+1)*max(Nx,Ny)), saveat=tstops, tstops=tstops)
